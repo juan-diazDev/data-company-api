ea24111ae9dc416f164eff2197ad0153c2bb34b0
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-20 23:18:47 -0500

    feat: add try/catch to index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 const express = require('express');
-const routes = require('./routes');
 
 const configExpress = require('./config/express');
+const routes = require('./routes');
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, async () => {
-  configExpress(app);
+  try {
+    configExpress(app);
 
-  routes(app);
+    routes(app);
 
-  console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
+  } catch (error) {
+    console.log({ ERROR: error.message });
+  }
 });
