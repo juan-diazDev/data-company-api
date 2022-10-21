6fc2166f7ea3dd806b4d3e06a63a071ee7b7b39d
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-20 23:18:12 -0500

    feat: add route for user module

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 const router = require('./api/checkroute');
+const users = require('./api/user/index');
 
 function routes(app) {
   app.use('/api/checkroute', router);
+
+  app.use('/api/user', users);
 }
 
 module.exports = routes;
