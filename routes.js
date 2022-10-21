896ab2703f52184809ac7026fa1463cbfae5fb3f
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 07:50:42 -0500

    feat: add companies route

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const router = require('./api/checkroute');
-const users = require('./api/user/index');
+const users = require('./api/user');
+const companies = require('./api/companies');
 
 function routes(app) {
   app.use('/api/checkroute', router);
 
   app.use('/api/user', users);
+  app.use('/api/companies', companies);
 }
 
 module.exports = routes;
