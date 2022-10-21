5501afa4eea528e5d7dbd0421b21deaeb231421e
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-20 23:17:41 -0500

    feat: add config for database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
@@ -12,24 +11,3 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 module.exports = sequelize;
-
-// const User = sequelize.define('user', {
-//   fullname: DataTypes.STRING,
-//   email: DataTypes.STRING,
-//   password: DataTypes.STRING,
-//   role: DataTypes.STRING,
-// });
-
-// async function main() {
-//   const juan = await User.create({
-//     fullname: 'Juanito Diaz',
-//     email: 'ju@g.com',
-//     password: '123',
-//     role: 'admin',
-//   });
-
-//   const users = await User.findAll();
-//   console.log('🚀 ~ file: db.js ~ line 30 ~ main ~ users', users);
-// }
-
-// main();
