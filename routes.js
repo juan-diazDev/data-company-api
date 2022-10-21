915d0969ca4393c594d3bf31c2c33c05230d381a
Juan Pablo Díaz <dhruva0108@gmail.com>
2022-10-21 08:20:02 -0500

    feat: add productos api route

--- routes.js
+++ routes.js
@@ -1,12 +1,14 @@
 const router = require('./api/checkroute');
 const users = require('./api/user');
 const companies = require('./api/companies');
+const products = require('./api/products');
 
 function routes(app) {
   app.use('/api/checkroute', router);
 
   app.use('/api/user', users);
   app.use('/api/companies', companies);
+  app.use('/api/products', products);
 }
 
 module.exports = routes;
